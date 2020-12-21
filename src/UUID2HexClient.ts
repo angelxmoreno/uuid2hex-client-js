17974438e40ceef7cd321144db09f4fd38b8e5cf
Angel X. Moreno <angelxmoreno@gmail.com>
2020-12-21 03:51:29 -0500

    lint fixes

--- src/UUID2HexClient.ts
+++ src/UUID2HexClient.ts
@@ -1,46 +1,45 @@
-import axios, {AxiosInstance} from "axios";
-import NullCacheEngine from "./NullCacheEngine";
-import {UUIDv4} from 'uuid-v4-validator'
+import axios, { AxiosInstance } from 'axios';
+import NullCacheEngine from './NullCacheEngine';
+import { UUIDv4 } from 'uuid-v4-validator';
 
 export default class UUID2HexClient {
-    serverUrl: string
-    http: AxiosInstance
-    cache: Cacheable
-    cachePrefix: string
-    
-    
+    serverUrl: string;
+    http: AxiosInstance;
+    cache: Cacheable;
+    cachePrefix: string;
+
     constructor(serverUrl: string, http?: AxiosInstance, cache?: Cacheable, cachePrefix?: string) {
         this.serverUrl = serverUrl;
         this.http = http || axios.create();
         this.cache = cache || new NullCacheEngine();
         this.cachePrefix = cachePrefix || 'UUID2HexClient';
     }
-    
+
     async getHex(uuid: string): Promise<string | null> {
         const isValidUUID = UUIDv4.validate(uuid);
         if (!isValidUUID) {
             throw new Error('Can not get hex for uuid: Invalid UUID provided');
         }
-    
+
         const cacheKey = this.cachePrefix + uuid.replace('_', '');
         const cachedResponse = await this.cache.getItem(cacheKey);
         if (cachedResponse) {
             return cachedResponse;
         }
-    
-        const response = await axios.post<ServerData>(`${this.serverUrl}`, {uuid});
-        const {statusCode, data} = response.data;
-        const {error, hex} = data;
-    
+
+        const response = await axios.post<ServerData>(`${this.serverUrl}`, { uuid });
+        const { statusCode, data } = response.data;
+        const { error, hex } = data;
+
         if (error || statusCode !== 200) {
-            throw new Error(`Can not get hex for uuid: ${error}`)
+            throw new Error(`Can not get hex for uuid: ${error}`);
         }
-    
+
         if (hex) {
             await this.cache.setItem(cacheKey, hex);
             return hex;
         }
-    
+
         return null;
     }
 }
